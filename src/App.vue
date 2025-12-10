e51b4a073a45cb1b9f478e18826f783de5df4db0
Franz Daniel Valsky <93553202+fdvalsky@users.noreply.github.com>
2025-12-11 00:28:59 +0100

    CSS classes added to App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@ import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <Navbar />
-  <RouterView />
-  <Footer></Footer>
+  <!-- VALSKY - added CSS classes in App.vue make sure that the footer component stays always on the bottom of the website -->
+  <div class="flex flex-col min-h-screen">
+    <Navbar />
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+    <Footer></Footer>
+  </div>
 </template>
 
 <style scoped></style>
